3f006a57ab858613305aac38b404fe27574de3fb
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-07-26 00:36:08 +0530

    feat: /uploadBase64 endpoint
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const Tesseract = require("tesseract.js")
 const app = express()
 
 app.use(fileUpload())
+app.use(express.json())
 
 app.get("/", (_, res) => {
-  res.send(`
+    res.send(`
     <form action='/upload' method='post' encType="multipart/form-data">
       <input type="file" name="sampleFile" />
       <input type='submit' value='Upload!' />
@@ -15,16 +16,39 @@ app.get("/", (_, res) => {
 })
 
 app.post("/upload", async (req, res) => {
-  const { sampleFile } = req.files
-  if (!sampleFile) return res.status(400).send("No files were uploaded.")
-  try {
-    const { data } = await Tesseract.recognize(sampleFile.data, "spa+eng", {
-      logger: (m) => console.log(m),
-    })
-    res.status(200).json({ text: data.text })
-  } catch (error) {
-    throw error
-  }
+    const { sampleFile } = req.files
+    if (!sampleFile) return res.status(400).send("No files were uploaded.")
+
+    const { data, error } = await doOcr(sampleFile.data)
+
+    if (data) return res.status(200).json({ text: data.text, error: null })
+
+    res.status(400).json({ error: error, text: null })
+
 })
+
+app.post("/uploadBase64", async (req, res) => {
+    
+    const { imageBase64 } = req.body
+    if (!imageBase64) return res.status(400).send("Image not found.")
+
+    const { data, error } = await doOcr(imageBase64)
+
+    if (data) return res.status(200).json({ text: data.text, error: null })
+
+    res.status(400).json({ error: error, text: null })
+})
+
+const doOcr = async (image) => {
+    try {
+        const { data } = await Tesseract.recognize(image, "spa+eng", {
+            logger: (m) => console.log(m),
+        })
+        return { data: data, error: null }
+    } catch (error) {
+        console.log(error);
+        return { data: null, error: error }
+    }
+}
 
 app.listen(process.env.PORT || 3000, () => console.log("Server started on port 3000"))
