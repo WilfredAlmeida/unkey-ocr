1b01a3a37c8f15f0bdf61daf896b5738a9ce9d02
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-07-26 01:09:10 +0530

    feat: /signup endpoint
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const fileUpload = require("express-fileupload")
 const Tesseract = require("tesseract.js")
+require("dotenv").config()
 
 const app = express()
 
@@ -28,7 +29,7 @@ app.post("/upload", async (req, res) => {
 })
 
 app.post("/uploadBase64", async (req, res) => {
-    
+
     const { imageBase64 } = req.body
     if (!imageBase64) return res.status(400).send("Image not found.")
 
@@ -37,6 +38,51 @@ app.post("/uploadBase64", async (req, res) => {
     if (data) return res.status(200).json({ text: data.text, error: null })
 
     res.status(400).json({ error: error, text: null })
+})
+
+app.post("/signUp", async (req, res) => {
+    const { name = "John Doe", email = "john@example.com" } = req.body
+
+    // Imaginary name and email validation
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${process.env.UNKEY_ROOT_KEY}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "apiId": process.env.UNKEY_API_ID,
+        "prefix": "ocr",
+        "byteLength": 16,
+        "ownerId": email,
+        "meta": {
+            "name": name,
+            "email": email
+        },
+        "expires": 1696870038000,
+        "ratelimit": {
+            "type": "fast",
+            "limit": 1,
+            "refillRate": 1,
+            "refillInterval": 1000
+        }
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    const createKeyResponse = await fetch("https://api.unkey.dev/v1/keys", requestOptions)
+    const createKeyResponseJson = await createKeyResponse.json()
+
+    if (createKeyResponseJson.error) return res.status(400).json({ error: createKeyResponseJson.error, keys: null })
+
+    return res.status(200).json({ keys: [createKeyResponseJson], error: null })
+
+
 })
 
 const doOcr = async (image) => {
